c23b65c6debd9b585c454070418fb23870cb588a
moadBili <charhbili.moad.etudes@gmail.com>
2020-12-22 03:02:27 +0100

    g

--- prod.py
+++ prod.py
@@ -1,5 +1,7 @@
 import urllib.request
 import json
+from kafka import KafkaProducer                                                                                         
+from time import sleep                                                                                                  
 
 def getResponse(url):
     operUrl = urllib.request.urlopen(url)
@@ -11,12 +13,19 @@ def getResponse(url):
     return jsonData
 
 def main():
-
+    BROKER = 'localhost:9092'                                                                                               
+    TOPIC = 'tweets' 
+    try:                                                                                                                    
+        p = KafkaProducer(bootstrap_servers=BROKER)                                                                         
+    except Exception as e:                                                                                                  
+        print(f"ERROR --> {e}")                                                                                             
+    sys.exit(1)       
     urlData = "http://vocab.nic.in/rest.php/states/json"
     jsonData = getResponse(urlData)
     # print the state id and state name corresponding
     for i in jsonData["states"]:
-        print(f'State Name:  {i["state"]["state_name"]} , State ID : {i["state"]["state_id"]}')
-
+        message=f'State Name:  {i["state"]["state_name"]} , State ID : {i["state"]["state_id"]}'
+        p.send(TOPIC, bytes(message, encoding="utf8"))                                                                      
+        sleep(randint(1,4))
 if __name__ == '__main__':
     main()
